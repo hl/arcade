80a64970b3fea1afc2309df7a235d9720733e3c9
Henricus Louwhoff <henricus@hey.com>
2023-01-04 22:25:28 +0200

    fix registry type key

--- lib/arcade_regions.ex
+++ lib/arcade_regions.ex
@@ -9,9 +9,11 @@ defmodule ArcadeRegions do
   alias ArcadeRegions.RegionDynamicSupervisor
   alias ArcadeRegions.RegionProcess
 
+  @registry_type :region
+
   def start_child(name, world_iid) when is_binary(name) and is_tuple(world_iid) do
-    num = HordeRegistry.next_key(:region, name)
-    iid = {:region, name, num}
+    num = HordeRegistry.next_key(@registry_type, name)
+    iid = {@registry_type, name, num}
 
     [iid: iid, name: name, world_iid: world_iid]
     |> RegionProcess.child_spec()
